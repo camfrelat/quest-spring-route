09f55592255aa89dfb20ee2f4a059a94dcb2f445
camille <camfrelat@gmail.com>
2021-12-21 11:38:10 +0100

    finished challenge

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,34 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
+	@GetMapping("/doctor/{number}")
+	@ResponseBody
+	public Doctor doctor(@PathVariable int number) {
 
-        return null;
-    }
+		// return a JSON with a new instance of Doctor
+		if (number == 13) {
+			return new Doctor(13, "Jodie Whittaker");
+		}
+
+		// thow error 303
+		else if (number >= 1 && number <= 12) {
+			throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Erreur 303");
+		}
+		// throw error 404 page not found
+		else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+		}
+
+	}
 }
